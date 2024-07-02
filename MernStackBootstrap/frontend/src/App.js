eb8b47a61c9db5c093935ac9b9ac1fd39266703d
asionsolver <asionsolver274@gmail.com>
2024-07-02 11:43:21 +0600

    implement change password features and logout features in our project

--- MernStackBootstrap/frontend/src/App.js
+++ MernStackBootstrap/frontend/src/App.js
@@ -3,15 +3,19 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { SignUp } from './components/SignUp';
 import { Login } from './components/Login';
+import ProtectedRoutes from './services/protectedRoutes';
 
 function App() {
   return (
     <div >
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+        
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/' element={<ProtectedRoutes />}>
+            <Route path="/" element={<Home />} />
+          </Route>
         </Routes>   
       </Router>
     </div>
